bcb6a06e072a873f19a54cb5666806f63ec8439b
fightctrl <sleepy.snoopy100@gmail.com>
2022-03-14 11:41:00 +0800

    week 3

---
+++ footer.php
@@ -0,0 +1,15 @@
+    <footer class="container-fluid text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>JOSH WHITKIN</p>
+                </div> <!-- col-md-6 -->
+                <div class="col-md-6">
+                    <p>Terms and Conditions</p>
+                </div> <!-- col-md-6 -->
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php get_header();?>
+    <section class="container-fluid aboutbg text-center">
+        <div class="container">
+            <h2>GAMES &amp; LEVEL DESIGNER</h2>
+            <p class="particle">...and particle wizard</p>
+            <p class="about-text">My name is Josh Whitkin. I'm a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
+            <div class="blocker"></div>
+        </div> <!-- container -->
+    </section>
+    <section class="container-fluid articlesbg">
+        <div class="container">
+            <h3 class="text-center">LATEST ARTICLES</h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/portrait1.jpg" alt="Illustration">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="date">Friday, November 10 2030</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+                    </p>
+                    <a class="readmore" href="#">CONTINUE READING</a>
+                </div> <!-- col-md-4 -->
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/portrait2.jpg" alt="Model">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="date">Friday, November 10 2030</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+                    </p>
+                    <a class="readmore" href="#">CONTINUE READING</a>
+                </div> <!-- col-md-4 -->
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh4/wp-content/themes/starter_for_josh_site/images/portrait3.jpg" alt="Track">
+                    <h4 class="article-title">A PORTRAIT</h4>
+                    <p class="date">Friday, November 10 2030</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+                    </p>
+                    <a class="readmore" href="#">CONTINUE READING</a>
+                </div> <!-- col-md-4 -->
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    <footer class="container-fluid text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>JOSH WHITKIN</p>
+                </div> <!-- col-md-6 -->
+                <div class="col-md-6">
+                    <p>Terms and Conditions</p>
+                </div> <!-- col-md-6 -->
+            </div> <!-- row -->
+        </div> <!-- container -->
+<?php get_footer();?>
